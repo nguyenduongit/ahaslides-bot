7586e8c73365dceb13c254048a34c9f165142886
Nguyễn Quốc Dương <88808137+nguyenduongit@users.noreply.github.com>
2025-07-03 00:30:19 +0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// File này chứa các biến cấu hình cho trang điều khiển.
-// KHÔNG ĐƯA FILE NÀY LÊN GITHUB.
-
-const CONFIG = {
-  SUPABASE_URL: "https://klkqhcmvsfcbindhodru.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtsa3FoY212c2ZjYmluZGhvZHJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0NzI2OTksImV4cCI6MjA2NzA0ODY5OX0.zgYtYET9saIdLGPeSvvWUdn8NQ5VUQ9ULmhynedHqvQ"
-};
